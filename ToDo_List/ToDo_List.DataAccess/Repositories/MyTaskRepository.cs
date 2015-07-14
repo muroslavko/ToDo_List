bc51fd57c0223bc1ead4f44f998124c51a289fec
muroslavko <myroslavdm@gmail.com>
2015-07-14 16:53:24 +0300

    Change Task -> MyTask

--- ToDo_List/ToDo_List.DataAccess/Repositories/MyTaskRepository.cs
+++ ToDo_List/ToDo_List.DataAccess/Repositories/MyTaskRepository.cs
@@ -4,12 +4,12 @@ using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToDo_List.DataAccess.Entities;
 using ToDo_List.DataAccess.Infrastructure;
-using Task = ToDo_List.DataAccess.Entities.MyTask;
 
 namespace ToDo_List.DataAccess.Repositories
 {
-    class MyTaskRepository : BaseRepository<Task>
+    class MyTaskRepository : BaseRepository<MyTask>
     {
         public MyTaskRepository(DbContext dbContext) : base(dbContext)
         {
